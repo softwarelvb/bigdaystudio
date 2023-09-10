cc275679385f0bef924d333257cb5508ad6ebb02
sindevs <sinkerd.dev@gmail.com>
2023-09-10 21:54:44 +0700

    update add firebase

--- lib/screens/UserProfile/UserProfile.dart
+++ lib/screens/UserProfile/UserProfile.dart
@@ -97,7 +97,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
                   const SizedBox(height: 16.0),
                   const Text(
                     'Email',
-                    style: const TextStyle(
+                    style: TextStyle(
                       fontSize: 16.0,
                       fontWeight: FontWeight.bold,
                     ),
